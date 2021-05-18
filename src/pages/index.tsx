861d6ff37df1f46ecb002e4ee70878d10269493a
Alan Gabriel <contatoalangabriel@gmail.com>
2021-05-18 11:31:03 -0300

    chore: configs initial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import Main from 'components/Main'
 
 export default function Home() {
-  return <Main />
+  return (
+    <Main
+      title="Central da felicidade"
+      description="Aqui vai ser o ponto de paz (ou não) do time"
+    />
+  )
 }
